21cc6a6c089125e78fe8b366eca4fa4df7a0d360
TaisGomess <163211069+TaisGomess@users.noreply.github.com>
2025-06-30 21:48:13 -0300

    minha parte

--- app/Models/ModelsAlunos/Aluno.js
+++ app/Models/ModelsAlunos/Aluno.js
@@ -1,18 +1,21 @@
-const Aluno = (sequelize, DataTypes) => {
-  return sequelize.define('Aluno', {
+
+import { DataTypes } from 'sequelize';
+import sequelize from '../../../config/sequelize.js';
+
+const Aluno =  sequelize.define('Aluno', {
+  aluno_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
     nome: {
       type: DataTypes.STRING,
       allowNull: false
-    },
-    idade: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    turma: {
-      type: DataTypes.STRING,
-      allowNull: false
     }
+  },{
+    tableName: 'alunos',
+    timestamps: false,
   });
-};
 
 export default Aluno;
